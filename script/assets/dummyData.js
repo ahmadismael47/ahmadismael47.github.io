fd1414b0f2bc0620a6ba3d567488883a2affc9ce
ahmadismael47 <ismaelahmad63@hotmail.com>
2022-08-28 06:12:24 +1200

    Added carousel, fix navigation

---
+++ script/assets/dummyData.js
@@ -0,0 +1,26 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+    icon: "fa-solid fa-house-user",
+  },
+  {
+    id: 2,
+    name: "About us",
+    path: "/",
+    icon: "fa-solid fa-people-group",
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/",
+    icon: "fa-solid fa-bug-slash",
+  },
+  {
+    id: 4,
+    name: "Products",
+    path: "/",
+    icon: "fa-solid fa-store",
+  },
+]
--- script/index.js
+++ script/index.js
@@ -1,3 +1,20 @@
+import { navLinks } from "./assets/dummyData.js"
+/////////////////////////////////////////NAV LINKS
+
+const linkContainer = document.querySelector(".navbar")
+const link = document.createElement("ul")
+
+link.innerHTML = navLinks
+  .map((item) => {
+    const { name, path, icon, id } = item
+    return ` <li><i class="${icon}"></i><a href="${path}">${name}</a></li>`
+  })
+  .join("")
+
+link.classList.add("links")
+linkContainer.append(link)
+
+//////////////////////////////TABS TABS TABS TABS TABS
 let tabs = document.querySelectorAll(".tab")
 const tabContainer = document.querySelector(".tab-container")
 const contentT = document.querySelectorAll(".content")
@@ -14,3 +31,76 @@ tabContainer.addEventListener("click", function (event) {
     .querySelector(`.content--${clicked.dataset.tab}`)
     .classList.add("content--active")
 })
+
+/////////////////////////////////SLIDE SLIDE SLIDE SLIDE
+
+const slides = document.querySelectorAll(".slide")
+const btnLeft = document.querySelector(".slider_btn--left")
+const btnRight = document.querySelector(".slider_btn--right")
+const dotContainer = document.querySelector(".dots")
+let currentSlide = 0
+let maxSlide = slides.length
+
+const createDots = function () {
+  slides.forEach(function (s, i) {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots_dot"  data-slide="${i}"></button>`
+    )
+  })
+}
+createDots()
+
+const activeDot = function (slide) {
+  document
+    .querySelectorAll(".dots_dot")
+    .forEach((dot) => dot.classList.remove("dots_dot--active"))
+
+  document
+    .querySelector(`.dots_dot[data-slide="${slide}"]`)
+    .classList.add("dots_dot--active")
+}
+
+const goToSlide = function (slide) {
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX(${150 * (i - slide)}%)`
+  })
+}
+goToSlide(0)
+
+const nextSlide = function () {
+  if (currentSlide === maxSlide - 1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  goToSlide(currentSlide)
+  activeDot(currentSlide)
+}
+const prevSlide = function () {
+  if (currentSlide === 0) {
+    currentSlide = maxSlide - 1
+  } else {
+    currentSlide--
+  }
+  goToSlide(currentSlide)
+  activeDot(currentSlide)
+}
+btnRight.addEventListener("click", nextSlide)
+btnLeft.addEventListener("click", prevSlide)
+
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots_dot")) {
+    const { slide } = e.target.dataset
+    goToSlide(slide)
+    activeDot(slide)
+  }
+})
+// slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`))
+
+// btnRight.addEventListener("click", function () {
+//   currentSlide++
+//   slides.forEach(
+//     (s, i) => (s.style.transform = `translateX(${100 * (i - currentSlide)}%)`)
+//   )
+// })
